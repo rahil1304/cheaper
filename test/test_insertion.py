6b324de81443bc2a717b896e251bc93c447ff6ce
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2021-09-06 14:08:15 -0400

    added test file and modified insertion accordingly

---
+++ test/test_insertion.py
@@ -0,0 +1,11 @@
+import code.insertion
+
+arr = code.insertion([4,5,3,1,2])
+
+#checking the length of the returned array
+assert(len(arr) == 5)
+
+#check if the returned array is in sorted order
+for i in range(1,len(arr)):
+    assert(arr[i-1] <= arr[i])
+
