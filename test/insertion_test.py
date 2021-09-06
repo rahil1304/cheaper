88d747728362d223c438b0fd1fe179b939e81464
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2021-09-06 15:13:30 -0400

    checking branch test

--- test/insertion_test.py
+++ test/insertion_test.py
@@ -1,11 +1,8 @@
 import sys
-# insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(1, 'E:/SE/project/cheaper/code')
 import insertion
 
 
-
-#from code import insertion
 arr = insertion.insertionSort([4,5,3,1,2])
 
 #checking the length of the returned array
