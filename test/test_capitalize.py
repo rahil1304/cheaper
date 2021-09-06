157a319451093c4ac90b2e4aa9cd1ce5d893ba22
Rahil Sarvaiya <rahil1304@gmail.com>
2021-09-06 15:07:51 -0400

    fixed conflicts

---
+++ test/test_capitalize.py
@@ -0,0 +1,10 @@
+def capitalize_string(s):
+    return s.capitalize()# test_capitalize.py
+
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
+
+def test_capitalize_string():
+    assert capitalize_string('test') == 'Test'
