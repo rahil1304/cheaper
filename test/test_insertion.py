a88a3060d20dc3433044eb240d70d8e2bf6c94e5
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2021-09-17 14:19:53 -0400

    final changes to test

--- test/test_insertion.py
+++ test/test_insertion.py
@@ -4,10 +4,17 @@ sys.path.append(os.path.abspath(os.path.join('..', 'code')))
 from code.insertion import *
 arr = insertionSort([4,5,3,1,2])
 
-#checking the length of the returned array
-assert(len(arr) == 5)
+def test_sort():
+    assert (check_sort(arr) and check_length(arr))
 
 #check if the returned array is in sorted order
-for i in range(1,len(arr)):
-    assert(arr[i-1] <= arr[i])
+def check_sort(l):
+    flag = 1
+    for i in range(1,len(l)):
+        if l[i-1] > l[i]:
+            flag = 0
+    return flag
 
+#checking the length of the returned array
+def check_length(l):
+    return len(l) == 5
